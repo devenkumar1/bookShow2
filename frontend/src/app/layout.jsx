f23b5a723124b77b8db03bde616b872fb4704933
devenkumar1 <devenkumar540@gmail.com>
2025-02-08 14:39:34 +0530

    fixed intial configs when loading next app

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -13,6 +13,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className="dark">
+      <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        </Head>
       <body className="antialiased">
         <Provider store={store}>
           <Toaster/>
